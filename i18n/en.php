fea2530be5caafd0b824b4d1b003743a420c7ec9
Nicolas Hurtubise <316k@legtux.org>
2016-08-09 11:05:35 -0400

    Translations

---
+++ i18n/en.php
@@ -0,0 +1,29 @@
+<?php
+return array(
+    'pagename-admin-board' => "Admin panel",
+    'success-edit' => "<b>:name</b> has been sucessfully modified !",
+    'success-delete' => "<b>:name</b> has been sucessfully deleted !",
+
+    'general.name' => 'Name',
+    'general.edit' => 'Edit',
+    'general.delete' => 'Delete',
+    'general.add' => 'Add',
+    'general.value' => 'Value',
+    'general.submit' => 'Submit',
+    'general.cancel' => 'Cancel',
+    'general.back' => 'Back',
+    'general.click-to-modify' => '(click to edit)',
+    
+    'general.file.uploaded' => "File uploaded with success",
+    'general.file.see' => "See",
+    
+    'model.user.email' => "Email",
+    'model.user.username' => "Username (used to login)",
+    'model.user.password' => "Password",
+    'model.user.logins' => "Number of logins",
+    'model.user.last_login' => "Last login date",
+    'model.user.has_many_through.name' => "",
+    'model.user.has_many_through.associations' => "",
+    'model.user.roles' => "Roles",
+);
+
