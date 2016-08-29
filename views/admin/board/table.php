30509276275747c40464b2939df914fe22c7105f
Nicolas Hurtubise <316k@legtux.org>
2016-08-29 18:23:42 -0400

    Fixes table view

--- views/admin/board/table.php
+++ views/admin/board/table.php
@@ -6,14 +6,13 @@
                     <?php echo __('model.' . strtolower($model_name) . '.table.' . $field); ?>
                 </th>
             <?php endforeach ?>
-            <th></th>
         </tr>
     </thead>
     <tbody>
         <?php foreach($models as $element): ?>
             <tr>
                 <?php foreach($details['list_fields'] as $field): ?>
-                    <td><?php echo $element[$field]; ?></td>
+                    <td><?php echo Arr::get($element, $field, '-'); ?></td>
                 <?php endforeach ?>
             </tr>
         <?php endforeach ?>
